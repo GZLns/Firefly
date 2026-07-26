ef0a245e16f1282f8d44f7a4cf3c22b1b956a5da
GZLns <13425121993@163.com>
2026-07-26 16:42:18 +0800

    fix: 更新 site_url 为 Cloudflare Pages 地址 114350234.pages.dev

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "全提瓦特索拉里斯海特洛可飞",
 
 	// 站点 URL
-	site_url: "https://gzlns-firefly.pages.dev",
+	site_url: "https://114350234.pages.dev",
 
 	// 站点描述
 	description:
